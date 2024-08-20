22d59c4c1c5f66560a14a543db528f42238dbd2e
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2024-08-20 17:18:27 -0300

    add script

--- script.js
+++ script.js
@@ -2,18 +2,42 @@ function mountOptionText(value) {
   return value < 10 ? "0" + value : value;
 }
 
-var hourSelect = document.getElementById("hora");
-Object.keys(10).forEach(function (index) {
+var hourElement = document.getElementById("hour");
+for (var index = 0; index < 24; index++) {
   var option = document.createElement("option");
   option.text = mountOptionText(index);
   option.value = index;
-  hourSelect.appendChild(option);
-});
+  hourElement.appendChild(option);
+}
 
-var minuteSelect = document.getElementById("minuto");
-Object.keys(10).forEach(function (index) {
+var minuteElement = document.getElementById("minute");
+for (var index = 0; index < 60; index++) {
   var option = document.createElement("option");
   option.text = mountOptionText(index);
   option.value = index;
-  minuteSelect.appendChild(option);
-});
+  minuteElement.appendChild(option);
+}
+
+let alarmTime, isAlarmSet, ringTone = new Audio("https://www.soundjay.com/button/beep-07.wav");
+
+console.log(alarmTime)
+
+let hours = null;
+let minutes = null;
+
+var date = new Date();
+var currentHours = date.getHours();
+var currentMinutes = date.getMinutes();
+
+if (alarmTime === `${hours}:${minutes}`) {
+  ringTone.play();
+  ringTone.loop = true;
+}
+
+function setAlarm() {
+  hours = document.getElementById("hour").value;
+  minutes = document.getElementById("minute").value;
+
+  alarmTime = `${hours}:${minutes}`;
+  isAlarmSet = true;
+}
